d6deb160e696a8019ca57745ae7df64a16864fbc
todo <todo@todo>
2021-09-03 10:47:17 +0800

    add sw log upload

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @RequestMapping("hello")
     public String hello() {
+        log.info("hello");
         return "hello";
     }
 
